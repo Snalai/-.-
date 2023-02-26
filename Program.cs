23ad3dc8ec57b406c1ac582de824edfe55b1126d
Alan <perrefsdd@mail.ru>
2023-02-26 15:21:55 +0300

    создал цикл который определяет длину второго массива

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿void funk(string[] Str)
-{   
+﻿
     int Len = Str.Length;
     int Widt = 0;
     int WidtNewStr = -1;
@@ -10,18 +9,6 @@
             Widt++;
         }
     }
-    string[] NewStr = new string[Widt];
-    for (int i = 0; i < Len; i++)
-    {
-        if (Str[i].Length <= 3)
-        {
-            WidtNewStr++;
-            NewStr[WidtNewStr] = Str[i];
-        }
-    }
-}
-string[] Str = { "hello", "2", "world", ":-)" };
-funk(Str);
 
 
 
