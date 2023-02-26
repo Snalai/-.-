af2d42253ffdb0b9bc847c12aee080929fcccfb9
Alan <perrefsdd@mail.ru>
2023-02-26 14:21:11 +0300

    Задача Создал счетчик определяющий длину следующего массива

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+ string[] Str = { "hello", "2", "world", ":-" };
+    int Len = Str.Length;
+    int Widt = 0;
+    int WidtNewStr = -1;
+    for (int i = 0; i < Len; i++)
+    {
+        if (Str[i].Length <= 3)
+        {
+            Widt++;
+        }
+    }
+    
+
+
+
+
+
+
